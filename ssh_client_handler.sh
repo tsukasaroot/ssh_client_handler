2eda27897f2d45d743f2e99b142f25c89e629b62
tsukasaroot <jordan.rouco-miguez@epitech.eu>
2020-07-07 15:55:38 +0200

    sending the script

---
+++ ssh_client_handler.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "No parameters given"
+	exit
+fi
+
+command=$(cat ssh_address.address | grep -w $1)
+echo "Looking for $1..."
+
+if [ ! -z "$command" ]
+then
+	IFS=' ' read -r id login <<< "$command"
+	#echo $id." foundn logging into SSH now..."
+	echo $login
+	$login
+else
+	echo "invalid website name"
+fi
